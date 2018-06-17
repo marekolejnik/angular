2b41be65f58f3eadad80469145990ca5b15b0bbe
Marek Olejnik <marekolejnik17@gmail.com>
2018-06-18 00:59:22 +0200

    lecture 22

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SucessAlertComponent } from './sucess-alert/sucess-alert.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SucessAlertComponent
   ],
   imports: [
     BrowserModule,
